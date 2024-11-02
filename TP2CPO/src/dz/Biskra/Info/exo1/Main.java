82bce068cb149944635fca81496b42b933afdaad
Hadeel2003-Gn <gounihadilasma@gmail.com>
2024-11-03 00:11:16 +0100

    Update Main.java

--- TP2CPO/src/dz/Biskra/Info/exo1/Main.java
+++ TP2CPO/src/dz/Biskra/Info/exo1/Main.java
@@ -1,15 +1,1 @@
-package dz.Biskra.Info.exo1;
 
-public class Main {
-	public static void main(String[] args) {
-        // Création d'une instance de GestionEtudiants
-        GestionEtudiants gestion = new GestionEtudiants();
-
-        // Ajout d'étudiants
-        gestion.ajouterEtudiant(new Etudiant("Gouni", "Hadeel Asma", 21, "05H03"));
-        gestion.ajouterEtudiant(new Etudiant("Lamri Sahraoui", "Lina Farah", 21, "04L03"));
-
-        // Affichage des étudiants
-        gestion.afficherEtudiants();
-    }
-}
