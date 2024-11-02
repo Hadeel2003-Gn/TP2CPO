ff10ac65c32a56f86e7b7016b0ef814e3157fb12
Hadeel2003-Gn <gounihadilasma@gmail.com>
2024-11-03 00:10:01 +0100

    hello

---
+++ TP2CPO/src/dz/Biskra/Info/exo1/Departement.java
@@ -0,0 +1,39 @@
+package dz.Biskra.Info.exo1;
+
+import java.util.ArrayList;
+
+public class Departement {
+    // Attributs privés
+    private String specialite;
+    private String adresse;
+    private ArrayList<Etudiant> etudiantsInscrits;
+
+    // Constructeur
+    public Departement(String specialite, String adresse) {
+        this.specialite = specialite;
+        this.adresse = adresse;
+        this.etudiantsInscrits = new ArrayList<>();
+    }
+
+    // Méthode pour inscrire un étudiant
+    public void inscrire(Etudiant etudiant) {
+        etudiantsInscrits.add(etudiant);
+    }
+
+    // Méthode pour désinscrire un étudiant
+    public void desinscrire(Etudiant etudiant) {
+        etudiantsInscrits.remove(etudiant);
+    }
+
+    // Méthode toString
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Département: ").append(specialite).append(", Adresse: ").append(adresse).append("\n");
+        sb.append("Étudiants inscrits :\n");
+        for (Etudiant etudiant : etudiantsInscrits) {
+            sb.append(etudiant.toString()).append("\n");
+        }
+        return sb.toString();
+    }
+}
--- TP2CPO/src/dz/Biskra/Info/exo1/Etudiant.java
+++ TP2CPO/src/dz/Biskra/Info/exo1/Etudiant.java
@@ -1,62 +1,43 @@
 package dz.Biskra.Info.exo1;
 
 
-	public class Etudiant {
-		// Attributs privés
-	    private String nom;
-	    private String prenom;
-	    private int age;
-	    private String matricule;
+import java.time.LocalDate;
 
-	    // Constructeur
-	    public Etudiant(String nom, String prenom, int age, String matricule) {
-	        this.nom = nom;
-	        this.prenom = prenom;
-	        this.age = age;
-	        this.matricule = matricule;
-	    }
+public class Etudiant {
+    // Attributs privés
+    private String nom;
+    private String prenom;
+    private LocalDate dateNaissance;
+    private String adresseMail;
+    private String adressePostale;
 
-	    // Getters
-	    public String getNom() {
-	        return nom;
-	    }
+    // Constructeur
+    public Etudiant(String nom, String prenom, LocalDate dateNaissance, String adresseMail, String adressePostale) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.dateNaissance = dateNaissance;
+        this.adresseMail = adresseMail;
+        this.adressePostale = adressePostale;
+    }
 
-	    public String getPrenom() {
-	        return prenom;
-	    }
+    // Getter et Setter pour le nom
+    public String getNom() {
+        return nom;
+    }
 
-	    public int getAge() {
-	        return age;
-	    }
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
 
-	    public String getMatricule() {
-	        return matricule;
-	    }
-
-	    // Setters
-	    public void setNom(String nom) {
-	        this.nom = nom;
-	    }
-
-	    public void setPrenom(String prenom) {
-	        this.prenom = prenom;
-	    }
-
-	    public void setAge(int age) {
-	        this.age = age;
-	    }
-
-	    public void setMatricule(String matricule) {
-	        this.matricule = matricule;
-	    }
-
-	    // Méthode pour afficher les informations de l'étudiant
-	    public void afficherInfo() {
-	        System.out.println("Nom: " + nom);
-	        System.out.println("Prénom: " + prenom);
-	        System.out.println("Âge: " + age);
-	        System.out.println("Matricule: " + matricule);
-	    }
-	}
-
-
+    // Méthode toString
+    @Override
+    public String toString() {
+        return "Etudiant{" +
+                "nom='" + nom + '\'' +
+                ", prenom='" + prenom + '\'' +
+                ", dateNaissance=" + dateNaissance +
+                ", adresseMail='" + adresseMail + '\'' +
+                ", adressePostale='" + adressePostale + '\'' +
+                '}';
+    }
+}
